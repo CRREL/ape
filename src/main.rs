358150429dbe9ec85f1e5af05abeea846f4a24da
Pete Gadomski <pete.gadomski@gmail.com>
2018-11-30 12:36:36 -0700

    Move sample to subcommand

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,14 @@ use std::fs::File;
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
-    let outfile = File::create(matches.value_of("OUTFILE").unwrap()).unwrap();
-    let config = Config::from_path(matches.value_of("CONFIG").unwrap()).unwrap();
-    let ape = ape::process(
-        config,
-        matches.value_of("FIXED").unwrap(),
-        matches.value_of("MOVING").unwrap(),
-    ).unwrap();
-    serde_json::to_writer(outfile, &ape).unwrap();
+    if let Some(matches) = matches.subcommand_matches("sample") {
+        let outfile = File::create(matches.value_of("OUTFILE").unwrap()).unwrap();
+        let config = Config::from_path(matches.value_of("CONFIG").unwrap()).unwrap();
+        let ape = ape::process(
+            config,
+            matches.value_of("FIXED").unwrap(),
+            matches.value_of("MOVING").unwrap(),
+        ).unwrap();
+        serde_json::to_writer(outfile, &ape).unwrap();
+    }
 }
